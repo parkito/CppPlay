5181ea0a8b6686bfe543a714b06c819ddf4cfdf5
parkito <artyom-karnov@yandex.ru>
2020-11-08 19:03:57 -0500

    Add boost library

--- cmake-tutorial/main.cpp
+++ cmake-tutorial/main.cpp
@@ -1,4 +1,7 @@
+#include <boost/random.hpp>
+
 #include "iostream"
+#include "math/operations.h"
 
 #ifndef CMAKEMACROSAMPLE
 #  define CMAKEMACROSAMPLE "NO SYSTEM NAME"
@@ -12,5 +15,22 @@ int main() {
   std::cout << "Hello CMake!" << std::endl;
   std::cout << CMAKEMACROSAMPLE << std::endl;
   std::cout << "Sum of 3 + 4 :" << sum(3, 4) << std::endl;
+
+  math::operations op;
+  int sum = op.sum(3, 4);
+
+  cout << sum;
+
+  // Boost Random Sample
+  boost::mt19937 rng;
+  double mean = 2.3;
+  double std = 0.34;
+  auto normal_dist = boost::random::normal_distribution<double>(mean, std);
+  boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > random_generator(
+      rng, normal_dist);
+  for (int i = 0; i < 2; i++) {
+    auto rand_val = random_generator();
+    std::cout << "Random Val " << i + 1 << " :" << rand_val << std::endl;
+  }
   return 0;
 }
---
+++ cmake-tutorial/math/operations.h
@@ -0,0 +1,14 @@
+#ifndef TUTORIAL_OPERATIONS_H
+#define TUTORIAL_OPERATIONS_H
+
+namespace math {
+  class operations {
+  public:
+    int sum(const int &a, const int &b);
+    int mult(const int &a, const int &b);
+    int div(const int &a, const int &b);
+    int sub(const int &a, const int &b);
+  };
+}  // namespace math
+
+#endif  // TUTORIAL_OPERATIONS_H
