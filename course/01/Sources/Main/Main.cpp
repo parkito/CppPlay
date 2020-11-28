fc2bc8cfa209f7b41c32491fc4e804ad41575bca
parkito <artyom-karnov@yandex.ru>
2020-11-28 15:25:02 -0700

    Implement functionality

--- course/01/Sources/Main/Main.cpp
+++ course/01/Sources/Main/Main.cpp
@@ -1,29 +1,82 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
+#include <algorithm>
+#include <array>
+#include<boost/tokenizer.hpp>
 
-int main(int argc, char **argv) {
-    std::cout << argc;
-    if (argc != 2) {
-        std::cerr << "You should enter the name of file\n";
-        exit(1);
-    }
-    std::ifstream file(argv[0]);
-    if (!file.is_open()) {
-        std::cerr << "Can open " << argv[0];
-        exit(1);
-    }
+std::vector<std::string> read_input();
 
-    std::vector<std::string> rawData;
-    std::string line;
-    while (getline(file, line)) {
-        rawData.push_back(line);
-    }
-    file.close();
+std::vector<std::array<std::string, 4>> format_input(std::vector<std::string> &data);
 
-    for (auto &el:rawData) {
-        std::cout << el;
-    }
+std::string format_line(std::string &str);
 
+void sort(std::vector<std::array<std::string, 4>> &data);
+
+void print(std::vector<std::array<std::string, 4>> &data);
+
+int main() {
+    auto input = read_input();
+    auto formattedInput = format_input(input);
+    sort(formattedInput);
+    print(formattedInput);
     return 0;
 }
+
+std::vector<std::string> read_input() {
+    std::vector<std::string> data;
+    for (std::string line; std::getline(std::cin, line);) {
+        data.push_back(line);
+    }
+    return data;
+}
+
+std::vector<std::array<std::string, 4>> format_input(std::vector<std::string> &data) {
+    std::vector<std::array<std::string, 4>> formattedData;
+    boost::char_separator<char> sep(".");
+    for (auto &rawLine:data) {
+        std::array<std::string, 4> lineArr;
+        auto line = format_line(rawLine);
+        boost::tokenizer<boost::char_separator<char>> tok(line, sep);
+        int curIndex = 0;
+        for (auto cur = tok.begin(); cur != tok.end(); ++cur) {
+            lineArr[curIndex++] = *cur;
+        }
+        formattedData.push_back(lineArr);
+    }
+
+    return formattedData;
+}
+
+std::string format_line(std::string &str) {
+    return str.substr(0, str.find('\t'));
+}
+
+void sort(std::vector<std::array<std::string, 4>> &data) {
+
+}
+
+void print(std::vector<std::array<std::string, 4>> &data) {
+    for (auto &line:data) {
+        for (int i = 0; i < 4; ++i) {
+            if (i != 3) {
+                std::cout << line[i] << ".";
+            } else {
+                std::cout << line[i] << std::endl;
+            }
+        }
+    }
+}
+
+
+//    std::ifstream file(argv[1]);
+//    if (!file.is_open()) {
+//        std::cerr << "Can open " << argv[0];
+//        exit(1);
+//    }
+//    while (getline(file, line)) {
+//    }
+//    file.close();
+
+//    std::ostringstream oss;
+//    std::copy(arr.begin(), arr.end(), std::ostream_iterator<std::string>(oss, "."));
+//    std::cout << oss.str() << std::endl;
---
+++ course/01/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat Resources/ip_filter.tsv | cmake-build-debug/ip_filter
