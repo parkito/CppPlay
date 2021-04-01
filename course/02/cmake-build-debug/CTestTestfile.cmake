63e288940e4043c59f20b8e0f62d9c342abfd76c
parkito <artyom-karnov@yandex.ru>
2021-03-31 21:09:53 -0600

    remove temporary dirs

--- course/02/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /code
-# Build directory: /code/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(allocator_test "allocator_test")
-set_tests_properties(allocator_test PROPERTIES  _BACKTRACE_TRIPLES "/code/CMakeLists.txt;73;add_test;/code/CMakeLists.txt;0;")
