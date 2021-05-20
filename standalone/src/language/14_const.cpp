674d7ae4f89ad92e09b48e1201dcee43683696bb
parkito <artyom-karnov@yandex.ru>
2021-05-19 23:23:42 -0400

    Add const example

---
+++ standalone/src/language/14_const.cpp
@@ -0,0 +1,36 @@
+
+class A {
+  // const methods do not allow to change class members
+  void method() const {
+    //    a=1;
+  }
+  int a;
+};
+
+// its special const that does not allow to reassignment
+void method(const int *a) {
+  a++;
+  //  a[0] = 1;
+  //  a = 1;
+}
+
+int main() {
+  // const make const an operator from the left or from the right if it const
+  // the first member of an expression
+
+  // pointers to const arrays
+  const int *a = new int[10];
+  int const *b = new int[10];
+
+  a = new int[20]; // ok
+  //  a[0] = 1; error
+  //  b[0] = 1; error
+
+  // constant pointers to array
+  int *const c = new int[10];
+  //  c = new int[10]; error
+  c[0] = 10; // ok
+
+  // constant pointer to to constant array
+  const int *const d = new int[10];
+}
--- standalone/src/patterns/creational/builder/Builders.cpp
+++ standalone/src/patterns/creational/builder/Builders.cpp
@@ -5,5 +5,9 @@ int main() {
   auto person1 =
       Person::builder().withId("id1").withName("name1").withAge(10).build();
 
-  std::cout << person1->getId();
+  std::cout << person1->getId() << std::endl;
+
+  auto person2 = Person::builder().withId("id1").withAge(10).build();
+
+  std::cout << person2->getName() << person2->getName().empty();
 }
