9f5c95b334d0edf6b413fc7bd273519e5ee26f77
parkito <artyom-karnov@yandex.ru>
2021-05-22 18:05:56 -0400

    Add 03 project

---
+++ course/03/Source/User.cpp
@@ -0,0 +1,5 @@
+//
+// Created by artyom.karnov on 5/22/21.
+//
+
+#include "User.h"
---
+++ course/03/Source/User.h
@@ -0,0 +1,14 @@
+//
+// Created by artyom.karnov on 5/22/21.
+//
+
+#ifndef DB_REST_PRJ_USER_H
+#define DB_REST_PRJ_USER_H
+
+
+class User {
+
+};
+
+
+#endif //DB_REST_PRJ_USER_H
