78e3873a57ce6253c4c544dccf2f0195663e807b
parkito <artyom-karnov@yandex.ru>
2021-05-27 00:30:05 -0400

    Add factory method start

---
+++ standalone/src/patterns/creational/factory_method/FactoryMethodTest.h
@@ -0,0 +1,8 @@
+//
+// Created by artyom.karnov on 5/27/21.
+//
+
+#ifndef CPPPLAY_FACTORYMETHODTEST_H
+#define CPPPLAY_FACTORYMETHODTEST_H
+
+#endif // CPPPLAY_FACTORYMETHODTEST_H
---
+++ standalone/src/patterns/creational/factory_method/Pizza.h
@@ -0,0 +1,8 @@
+//
+// Created by artyom.karnov on 5/27/21.
+//
+
+#ifndef CPPPLAY_PIZZA_H
+#define CPPPLAY_PIZZA_H
+
+#endif // CPPPLAY_PIZZA_H
